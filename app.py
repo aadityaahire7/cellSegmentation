5b6795a8f2facf5ff966a392fd8f5eca200f5462
aadityaahire <aadityaahire12@gmail.com>
2024-08-23 16:36:37 +0530

    data injestion done

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from cellSegmentation.logger import logging
-from cellSegmentation.exception import AppException
-import sys
+import sys,os
+from cellSegmentation.pipeline.training_pipeline import TrainPipeline
 
-logging.info("Welcome To the Custom Log")
 
-try:
-    a=4/"6"
 
-except Exception as e:
-    raise AppException(e,sys)
+obj=TrainPipeline()
+obj.run_pipeline()
+print("Training Done")
+
+
